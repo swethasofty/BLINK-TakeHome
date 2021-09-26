0cebaeabbe676a5a1afff4633018ca3ab931941f
swethasofty <swethatm.cs@gmail.com>
2021-09-26 15:33:05 -0700

    Create Router.js
    First Commit for BLIBK Take home

---
+++ src/Router.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { Router, Route,Switch} from "react-router-dom";
+import Drugs from "./Components/Drugs";
+import DrugsDetails from "./Components/DrugsDetails";
+import { createBrowserHistory } from "history";
+
+const history = createBrowserHistory();
+
+
+class RouterApp extends Component {
+  render() {
+    return (
+      <Router history={history}>
+        <Switch>
+         <Route exact path="/" component={Drugs} />
+          <Route path="/drugs/:drug_name?/:rxcui?/:synonym?" component={DrugsDetails} />
+          </Switch>
+      </Router>
+    );
+  }
+}
+
+export default RouterApp;
